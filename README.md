eeb53da6bc486756d142696ce22e114e6d0aa233
Michael Soriano <mks6804@gmail.com>
2019-11-27 11:23:26 -0800

    updated readme with instructions for venv

---
+++ README.md
@@ -0,0 +1,22 @@
+## Create VENV:
+
+In Git Bash: 
+
+`virtualenv venv`
+
+- then activate: 
+
+`. Scripts/activate`
+
+
+## IN VSCode
+
+* add venvPath in VScode settings:
+
+`"python.venvPath": "C:\\Users\\USERNAME\\python-projects\\"`
+
+* add Python Interpreter 
+
+- Do CTRL + SHIFT + P
+- Python: Select Interpreter
+- A list of venv python.exe should show up
--- flask-venv1/settings.py
+++ flask-venv1/settings.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\users\\mks6804\\python-projects\\flask\\database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\users\\mks6804\\python-projects\\flask-venv1\\database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'seckey'
