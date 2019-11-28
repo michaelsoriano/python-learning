3b25685082e0865c24748fbb9bdf9ba7e2576361
Michael Soriano <mks6804@gmail.com>
2019-11-28 13:27:26 -0800

    added new app player, templates

---
+++ django-venv1/tictactoe/player/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from gameplay.models import Game
+
+def home(request):
+    return render(request, "player/home.html", 
+        {'ngames' : Game.objects.count()})
